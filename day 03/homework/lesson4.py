596f455d47f37ba37b5b6fb2db079a44cbf3f806
ErekleKilasonia <ereklekilasonia2010@gmail.com>
2024-03-17 11:17:00 +0400

    commit

---
+++ day 03/homework/lesson4.py
@@ -0,0 +1,16 @@
+num1 = 38
+num2 = 44
+num3 = 10
+num4 = 3
+num5 = "years old"
+
+print("My mother is" + " " + str(num1) + " " + "My father is" + " " + str(num2) + " " + "My brother is" + " " + str(num3) + " " + "My sister is" + " " + str(num4) + " " + num5)
+
+
+
+num1 = 40             #after two years
+num2 = 46             
+num3 = 12
+num4 = 5
+
+print("After 2 years my mother would be" + " " + str(num1) + " " + "My father " + " " + str(num2) + " " + "My brother " + " " + str(num3) + " " + "My sister" + " " + str(num4) + " " + num5)
--- lesson4.py
+++
@@ -1,7 +0,0 @@
-num1 = 38
-num2 = 44
-num3 = 10
-num4 = 3
-num5 = "years old"
-
-print("My mother is + " " + str(num1) + " " + My father is + " " + str(num2) + " " + My brother is + " " + str(num3) + " " + My sister is + " " + str(num4) + " " + num5")
